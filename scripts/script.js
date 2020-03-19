ca7270a9a84627aa261605ddc00591f833c56bf1
Obylisk <obylisk0gmail.com>
2020-03-19 13:53:16 +0700

    Created 16X16 grid

---
+++ scripts/script.js
@@ -0,0 +1,9 @@
+let i = 0;
+let x = 16*16
+
+for (i; i < x; i++) {
+  let grid = document.createElement("div");
+  grid.classList.add("grid");
+  document.getElementById("container");
+  container.appendChild(grid);
+};
