fd0e5354c9615eee4dbb66d4bb6743cbd90c6f3d
Obylisk <obylisk0gmail.com>
2020-03-19 21:59:50 +0700

    Completed Etch a Sketch

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,39 @@
-let i = 0;
-let x = 16*16
+const container =   document.getElementById("container");
 
-for (i; i < x; i++) {
+makeGrid(16);
+reset();
+
+function makeGrid(x){
   let grid = document.createElement("div");
   grid.classList.add("grid");
-  document.getElementById("container");
-  container.appendChild(grid);
-};
+  //container.appendChild(grid);
+  for (let i = 0; i < x; i++) {
+    let row = document.createElement("div");
+    row.className = "row";
+    for (let j = 1; j <= x; j++) {
+      let cell = document.createElement("div");
+      cell.className = "gridSquare";
+      cell.addEventListener("mouseenter", (e) => {e.target.style.backgroundColor = "blue";})
+      cell.addEventListener("click", (e) => {e.target.style.backgroundColor = "white";})
+      row.appendChild(cell);
+    };
+    container.appendChild(row);
+    }
+
+}
+
+function reset() {
+  let button = document.querySelector("button");
+  button.addEventListener("click", resetGrid, false);
+}
+
+function resetGrid(){
+  container.innerHTML = "";
+  let cellNumber = prompt("Selet grid size", 16);
+  if (cellNumber >= 10 && cellNumber <= 50) {
+    makeGrid(cellNumber);
+  } else {
+    alert("Select a number between 10 and 50.")
+    resetGrid();
+  }
+}
